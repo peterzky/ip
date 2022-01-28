7071766d1caa0a14d78d312f4e21f4c0d76b7b67
peterzky <378096232@qq.com>
2022-01-28 16:15:28 +0800

    initial commit

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+download_apnic() {
+    echo "Downloading China IPs from APNIC"
+    curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | \
+	grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' | \
+	iprange --optimize > cnip.txt
+    echo "Apnic download completed"
+}
+
+download_apnic
